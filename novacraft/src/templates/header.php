0f3063c5e98034408072c540f4895698f74ba1ce
yakhlafhoussam <yakhlafhoussam@gmail.com>
2025-12-19 15:05:14 +0100

    Edit logic contact to get the user info if he's log in

--- novacraft/src/templates/header.php
+++ novacraft/src/templates/header.php
@@ -9,11 +9,9 @@
         <a id="contact" class="font-inter font-bold text-xl text-white hover:scale-105" href="contact">Contact</a>
     </div>
     <?php
-    session_start();
     if (isset($_SESSION["id"])) {
         echo "<a href='profile' class='bg-white py-3 w-32 text-center rounded-full font-inter font-bold text-xl text-black hover:bg-gray-200 mr-20'>My profile</a>";
     } else {
-        session_destroy();
         echo '<div class="h-full flex justify-around items-center gap-2">
                 <a href="login" class="bg-white py-3 w-32 text-center rounded-full font-inter font-bold text-xl text-black hover:bg-gray-200">Log in</a>
                 <a href="register" class="py-3 w-32 text-center rounded-full font-inter font-bold text-xl border border-solid border-white text-white hover:bg-gray-800">Sign UP</a>
